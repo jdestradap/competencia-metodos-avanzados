b181fb668dc2003c243e02c1dde19426f5c091bf
Juan Estrada <jdestradap@gmail.com>
2019-09-08 19:53:15 -0500

    corriendo glmnet con binomial

--- competencia_v1.R
+++ competencia_v1.R
@@ -1,12 +1,12 @@
-# Function to extract a prepare the data
-
+library(caret)
+library(glmnet)
 gettingTheData <- function(filePath) {
   data <- na.omit(read.table(filePath, header = TRUE, sep = ","))
 }
 
 df <- gettingTheData("databinarystudents.csv")
 
-ind_train <- sample(1:nrow(df), size = round(0.8*nrow(df)))
+ind_train <- sample(1:nrow(df), size = round(1*nrow(df)))
 
 train <- df[ind_train,]
 
@@ -18,6 +18,22 @@ x.train.scale <- scale(train[,3:34])
 
 y.test <- test[,2]
 
+# Set training control
+train_control <- trainControl(method = "repeatedcv",
+                              number = 5,
+                              repeats = 5,
+                              search = "random",
+                              verboseIter = TRUE)
 
-# 
+# Train the model
+y.train.factor <- as.factor(y.train)
 
+elastic_net_model <- train(x.train.scale,y.train.factor,
+                           method = "glmnet",
+                           preProcess = NULL,
+                           tuneLength = 25, # Amount of granularity in the tuning parameter grid
+                           trControl = train_control,
+                           family="binomial")
+
+md3 <- glmnet(x.train.scale, y.train, family = 'binomial', lambda = 0.240651637, alpha = 0.12921161)
+coef(md3)
